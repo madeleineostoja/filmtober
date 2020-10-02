7bbc42f8b9a1803c65a5bb30f422e22f8d684627
Sean King <sean@seanking.co>
2020-10-02 21:37:05 +1300

    Add external link tracking

---
+++ src/components/ExternalLink.svelte
@@ -0,0 +1,18 @@
+<!--
+  ExternalLink Component
+  Simple external link wrapper
+-->
+<script lang="ts">
+  /** URL to link to */
+  export let href: string;
+
+  function trackLink() {
+    gtag('event', 'click', {
+      event_category: 'outbound',
+      event_label: href,
+      transport_type: 'beacon'
+    });
+  }
+</script>
+
+<a {href} on:click={trackLink} target="_blank" rel="noopener"><slot /></a>
